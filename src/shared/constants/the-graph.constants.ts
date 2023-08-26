8c9f8b14b6f3f4f17d3b07ec7ffd2af393497a3c
Stx69 <rudisek@seznam.cz>
2023-08-26 20:13:42 +0200

    graph forge api udpated

--- src/shared/constants/the-graph.constants.ts
+++ src/shared/constants/the-graph.constants.ts
@@ -1,14 +1,7 @@
-// export const GRAPH_CORE_API: string =
-//   'https://subgraph.satsuma-prod.com/tWYl5n5y04oz/aavegotchi/aavegotchi-core-matic/api';
-
-// export const GRAPH_GOTCHI_SVG_API: string = 
-//'https://api.thegraph.com/subgraphs/name/aavegotchi/aavegotchi-svg';
-
 export const GRAPH_CORE_API: string =
   'https://subgraph.satsuma-prod.com/tWYl5n5y04oz/aavegotchi/aavegotchi-core-matic/version/v2.0.1/api';
-
 export const GRAPH_RAFFLE_API: string = 'https://api.thegraph.com/subgraphs/name/froid1911/aavegotchi-raffles';
-export const GRAPH_GOTCHI_SVG_API: string = 'https://subgraph.satsuma-prod.com/tWYl5n5y04oz/aavegotchi/aavegotchi-svg-matic/api';
+export const GRAPH_GOTCHI_SVG_API: string = 'https://api.thegraph.com/subgraphs/name/aavegotchi/aavegotchi-svg';
 export const GRAPH_REALM_API: string = 'https://api.thegraph.com/subgraphs/name/aavegotchi/aavegotchi-realm-matic';
 export const GRAPH_GOTCHIVERSE_API: string = 'https://api.thegraph.com/subgraphs/name/aavegotchi/gotchiverse-matic';
 export const GRAPH_INCOME_API: string = 'https://api.thegraph.com/subgraphs/name/nicolasnin/gotchiincome';
@@ -17,4 +10,4 @@ export const GRAPH_FIREBALL_MAIN_API: string = 'https://api.thegraph.com/subgrap
 export const GRAPH_FIREBALL_API: string = 'https://api.thegraph.com/subgraphs/name/orden-gg/gotchiverse';
 export const GRAPH_FIREBALL_FORGE_API: string = 'https://api.thegraph.com/subgraphs/name/orden-gg/gotchi-forge';
 
-export const MAX_BATCH_QUERIES = 300;
+export const MAX_BATCH_QUERIES = 350;
